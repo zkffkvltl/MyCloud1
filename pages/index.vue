587c4be74885ec467b2b868b467adfad7d54218d
zkffkvltl <zkffkvltl@gmail.com>
2018-06-19 12:50:19 +0900

    test

--- pages/index.vue
+++ pages/index.vue
@@ -9,60 +9,152 @@
    </div>
 </div>
 
-<nav class="navbar">
+<nav class="navbar is-black">
   <div class="navbar-menu">
     <div class="navbar-start">
       <a class="navbar-item" href="http://www.oins.ml">
         Home
       </a>
-    <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link" href="http://www.oins.ml/about_1">
-          Docs
+      <a class="navbar-item" href="http://www.oins.ml/page/about_1/">
+         Test_1
         </a>
-    </div>
-  </div>
+      <a class="navbar-item" href="http://www.oins.ml/page/about_2/">
+         Test_2
+      </a>
+   </div>
+ </div>
 </nav>
 
-
-<div class="message is-dark">
-  <div class="message-header">
-  </div>
-  <div class="container is-widescreen">
-  <div class="notificatio">
-
-
-<div class="columns is-mobile has-text-centered">
-  <div class="column">
-    
-    <figure class="image is-128x128">
-        <img src="~static/console.png" alt="">
-    </figure>
-    <div class="content">
-    <p class="">1</p>
+ 
+  <div class="container is-fluid">
+    <div class="hero is-medium is-light">
+    <div class="notificatio">
+      
+      <br>
+      <br>
+      <br>
+      <h1>이 포트폴리오는 <strong>클라우드 서비스</strong> 수업을 참고하여 제작되었습니다. </h1>
+      <br>
+      <p>
+        컴퓨터를 전공으로 삼은 학생이 흥미 위주로 제작, 코딩한 제작물을 기록하고자 이러한 홈페이지를 개설하였으며, <br> 제작물 외 따로 저장하고자 하는 정보를 넣기 및 실습을 하려합니다.
+      </p>
+      <br>
+      <br>
+      <br>
     </div>
   </div>
+ </div>
+
+<br>
+
+<div class="columns is-mobile has-text-centered">
+
   <div class="column">
-    <figure class="image is-128x128">
-
-    </figure>
-    2
+    <div class="content is-small">
+      <figure class="image is-128x128">  
+         <img src="~static/console.png" align="center">
+      </figure>
+    <h2>게임에 대한 흥미</h2>
+    <p>게임을 하는 것 뿐만 아닌 제작에도 많은 흥미가 있습니다.<br> 간단한 도트 그래픽의 슈팅게임에서부터 3D 그래픽의 롤플레잉게임, 서바이벌 장르에서 FPS 장르까지, 모바일의 온라인 게임부터 게임팩으로 출시되는 싱글게임, VR기기와 플레이스테이션 독점게임 등등 게임 플레이에 대한 많은 흥미를 가지고 있을 뿐만 아니라 게임의 구성, 시나리오의 설계 등 여러 방면으로 관심을 가지고 있습니다.<br> 현재는 좀 더 넓은 시야로 게임 자체를 바라보도록 노력하고 있습니다. </p>
+    </div>
   </div>
+
   <div class="column">
+   <div class="content is-small">
+     <figure class="image is-128x128">  
+        <img src="~static/coding.png" align="center">
+      </figure>
+    <h2>여러 컴퓨터 언어 공부 중</h2>
+    <p>C언어, PHP, HTML를 활용할 수 있으며, 현재 JAVA와 MFC를 공부 중입니다. 아직 실력은 미흡하지만 실습을 통해 점차 언어에 익숙해질 예정입니다.</p>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="content is-small">
+      <figure class="image is-128x128"> 
+        <img src="~static/thought.png" align="center">
+      </figure>
+    <h2>스스로 실습하기</h2>
+    <p>직접 언어를 공부하기는 힘듭니다. 따라서 인터넷에 퍼져있는 여러 자료들을 참고하여 스스로 실습을 해보도록 하고 있습니다. JAVA로 만드는 쉬운 슈팅게임 또는 MYSQL를 사용한 PHP 게시판 등을 홀로 공부하고 있으며, 앞으로도 많은 제작을 하려할 것입니다. 중도 포기하지 않도록 되도록이면 흥미를 가지고 있는 주제로 생각하고 있습니다.</p>
+   </div>
+  </div>
+
+</div>
+
+<br>
+
+<div class="hero is-large is-light is-bold">
+
+<br>
+<br>
+
+<div class="columns is-mobile">
+
+<div class="column is-one-fifth">
+  <br>
+</div>
+
+
+<div class="column">
+<div class="card">
+  <div class="card-image">
     <figure class="image is-128x128">
-
+      <img src="~static/blog.png" alt="">
     </figure>
-    3
   </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-content">
+        <p class="title is-4">PHP 게시판</p>
+        <p class="subtitle is-6">MYSQL를 사용한 미니 게시판</p>
+      </div>
+    </div>
+
+    <div class="content">
+      MYSQL의 데이터베이스와 연결하여 값을 집어넣고 가져오는 방법을 습득했다. 개인으로 스스로 처음 제작해본 게시판.
+      <br>
+      <time datetime="2017">2017</time>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="column">
+<div class="card">
+  <div class="card-image">
+    <figure class="image is-128x128">
+      <img src="~static/shooting-star.png" alt="">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-content">
+        <p class="title is-4">JAVA 슈팅게임</p>
+        <p class="subtitle is-6">자바언어를 사용한 간단한 슈팅게임</p>
+      </div>
+    </div>
+
+    <div class="content">
+      자바로 게임을 구현해볼 수 있었다. 플레이어와 적, 그들의 충돌과 그 범위를 구현하는 방법, 적의 무빙패턴, 플레이어의 총알 발사 구현, 아이템 등 게임 구현의 기초를 이해할 수 있었다. 두 명의 팀원과 같이 개발해보았다.
+      <br>
+      <time datetime="2018">2018</time>
+    </div>
+  </div>
+</div>
+</div>
+
+
+<div class="column is-one-fifth">
+  <br>
+</div>
 
 </div>
 
-  </div>
-  </div>
+<br>
+<br>
+
 </div>
 
-<div class="colums">
- 
-</div>
 <!--<div>
       <app-logo/>
       <h1 class="title">
